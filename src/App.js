323b53f60952f4f9be854df3c6edd16fe9b8381f
camilevahviraki <camilevahviraki@gmail.com>
2022-07-30 16:19:54 +0200

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Items from './modules/Categories/categories';
+import Cart from './modules/Cart/Cart';
+import Header from './modules/Header/header';
+import Product from './modules/Details/product';
+import store from './Redux/Store';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencieId: 0,
+      showMiniCArt: false,
+      productId: null,
+      cartShown: false,
+    };
+  }
+
+  changHeaderLink = () => {
+    this.setState({
+      showMiniCArt: false,
+    });
+  }
+
+  selectCurrency = (e) => {
+    switch (e.target.value) {
+      case 'USD':
+        return this.setState({ currencieId: 0 });
+      case 'GBP':
+        return this.setState({ currencieId: 1 });
+      case 'AUD':
+        return this.setState({ currencieId: 2 });
+      case 'JPY':
+        return this.setState({ currencieId: 3 });
+      case 'RUB':
+        return this.setState({ currencieId: 4 });
+      default:
+        return this.setState({ currencieId: 0 });
+    }
+  }
+
+  seeDetails = (id) => {
+    this.setState({productId: id})
+    console.log(id);
+  }
+
+  showMiniCArt = (bool) => {
+    this.setState({showMiniCArt: bool})
+  }
+
+  // cartShown = (bool) => {
+  //   this.setState({cartShown: bool});
+  // }
+  
+
+  render() {
+    console.log("show mini cart", this.state.showMiniCArt)
+    console.log(this.state.productId)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <BrowserRouter>
+      <Header
+        changeFilter={this.changHeaderLink}
+        selectCurrency={this.selectCurrency}
+        currencieId={this.state.currencieId}
+        cartShown={this.state.showMiniCArt}
+        showMiniCArt={this.showMiniCArt}
+      />
+      <Routes>
+        <Route path="/" element={
+          <Items
+            filter={0}
+            currencieId={this.state.currencieId}
+            grayFilter={this.state.showMiniCArt}
+            hideMiniCart={this.showMiniCArt}
+            details={this.seeDetails}
+          />} />
+        <Route path="/clothes" element={
+          <Items
+            filter={1}
+            currencieId={this.state.currencieId}
+            grayFilter={this.state.showMiniCArt}
+            hideMiniCart={this.showMiniCArt}
+            details={this.seeDetails}
+          />} />
+        <Route path="/tech" element={
+          <Items
+            filter={2}
+            currencieId={this.state.currencieId}
+            grayFilter={this.state.showMiniCArt}
+            hideMiniCart={this.showMiniCArt}
+            details={this.seeDetails}
+          />} />
+        <Route path="details" element={
+                  <Product
+                  productId={this.state.productId}
+                  currencieId={this.state.currencieId}
+                  showMiniCArt={this.showMiniCArt}
+                  grayFilter={this.state.showMiniCArt}
+                />
+        } />
+        <Route path="/cart" element={<Cart 
+          currencieId={this.state.currencieId}
+          cartShown={this.showMiniCArt}
+        />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
   );
+}
 }
 
 export default App;
