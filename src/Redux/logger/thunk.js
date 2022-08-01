07b22b9d05777012987a39fe941c947cc3e4665c
camilevahviraki <camilevahviraki@gmail.com>
2022-08-01 18:11:28 +0200

    Update thunk.js in redux folder

--- src/Redux/logger/thunk.js
+++ src/Redux/logger/thunk.js
@@ -1,11 +1,9 @@
 const thunk = ({ dispatch, getState }) => (next) => (action) => {
-  // If the "action" is actually a function instead...
+
   if (typeof action === 'function') {
-    // then call the function and pass `dispatch` and `getState` as arguments
+
     return action(dispatch, getState);
   }
-
-  // Otherwise, it's a normal action - send it onwards
   return next(action);
 };
 
