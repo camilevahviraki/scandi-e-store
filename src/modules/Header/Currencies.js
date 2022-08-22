eba8e93adc876a218dcc1cef4f19af6ae652457a
camilevahviraki <camilevahviraki@gmail.com>
2022-08-22 23:58:07 +0200

    Refactor the currencies to remove hardcode

--- src/modules/Header/Currencies.js
+++ src/modules/Header/Currencies.js
@@ -1,44 +1,76 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import arrow from '../../icons/Vector-currency.png';
+import '../../styles/currencies.css';
+import { fetchCurrencies } from '../../Redux/currencies/currencies';
 
-class Currencie extends Component {
+class Currency extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currencies: [
-        { s: '$', id: 'USD' },
-        { s: '£', id: 'GBP' },
-        { s: 'A$', id: 'AUD' },
-        { s: '¥', id: 'JPY' },
-        { s: '₽', id: 'RUB' },
-      ],
-      showCurrencie: false,
+      showCurrency: false,
+      selected: null,
+      active: 0,
     };
+  }
+
+  componentDidMount() {
+    this.props.fetchCurrencies();
   }
 
   render() {
     return (
-      <select
-        name="currencies"
-        className="currencies"
-        onChange={this.props.selectCurrency}
-        onMouseOver={() => this.setState({ showCurrencie: true })}
-        onMouseLeave={() => this.setState({ showCurrencie: false })}
-      >
-        {
-            this.state.currencies.map((currency) => (
-              <option
-                value={currency.id}
-                key={currency.id}
+      <div className="currencies-wrap">
+
+        <div
+          className="choose-currency"
+          onClick={() => this.setState({ showCurrency: !this.state.showCurrency })}
+        >
+          {' '}
+          {
+        this.state.selected
+          ? (
+            <p>{this.state.selected}</p>
+          )
+          : (
+            <p>
+              {this.props.currencies.length > 0
+                ? this.props.currencies[0].symbol : ''}
+            </p>
+          )
+      }
+          <img src={arrow} alt="" className={this.state.showCurrency ? 'arrow' : 'arrow arrow-down'} />
+        </div>
+        <div className="currencies" style={this.state.showCurrency ? { display: 'flex' } : { display: 'none' }}>
+          { this.props.currencies.length > 0
+            ? this.props.currencies.map((currency, id) => (
+              <p
+                className={this.state.active === id ? 'selected-currency' : 'unselsected-curr'}
+                onClick={() => {
+                  this.setState({
+                    showCurrency: false,
+                    selected: currency.symbol,
+                    active: id,
+                  });
+                  this.props.selectCurrency(id);
+                }}
+                key={currency.label}
               >
-                {currency.s}
+                {currency.symbol}
                 {' '}
-                {this.state.showCurrencie ? currency.id : ''}
-              </option>
+                {currency.label}
+              </p>
             ))
-          }
-      </select>
+            : ''}
+        </div>
+      </div>
     );
   }
 }
 
-export default Currencie;
+const mapState = (state) => ({ currencies: state.currenciesReducer });
+const mapDispatch = (dispatch) => ({
+  fetchCurrencies: () => dispatch(fetchCurrencies()),
+});
+
+export default connect(mapState, mapDispatch)(Currency);
