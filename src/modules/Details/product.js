d40e0312ea87196ba34a2a0b41d3759b55dd11c9
camilevahviraki <camilevahviraki@gmail.com>
2022-08-22 23:57:24 +0200

    Update product details page

--- src/modules/Details/product.js
+++ src/modules/Details/product.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchProduct } from '../../Redux/product/product';
 import { addTocard } from '../../Redux/cart/cart';
-import { changeStoredStatus } from '../../Redux/categories/categories';
+import Description from './description';
 import '../../styles/product.css';
 
 class Product extends Component {
@@ -12,6 +12,10 @@ class Product extends Component {
       imageShown: 0,
       colorSelected: 0,
       sizeSelected: 0,
+      sizeSelected0: 0,
+      sizeSelected1: 0,
+      sizeSelected2: 0,
+      message: '',
     };
   }
 
@@ -33,139 +37,180 @@ class Product extends Component {
     }
   }
 
-      addNewToCart = (newProduct) => {
-        let cartProduct = newProduct;
-        const { colorSelected } = this.state;
-        const { sizeSelected } = this.state;
-        cartProduct = { ...cartProduct, colorSelected };
-        cartProduct = { ...cartProduct, sizeSelected };
-        this.props.addTocard(cartProduct);
-        this.setState({ colorSelected: 0, sizeSelected: 0 });
-        this.props.changeStoredStatus(newProduct.name);
-        this.props.showMiniCArt(true);
-      }
+  addNewToCart = (newProduct) => {
+    if (newProduct.inStock) {
+      let cartProduct = newProduct;
+      const { colorSelected } = this.state;
+      const { sizeSelected0 } = this.state;
+      const { sizeSelected1 } = this.state;
+      const { sizeSelected2 } = this.state;
+      cartProduct = {
+        ...cartProduct,
+        colorSelected,
+        sizeSelected0,
+        sizeSelected1,
+        sizeSelected2,
+      };
+      this.props.addTocard(cartProduct);
+      this.props.changeStoredStatus(newProduct.name);
+      this.props.showMiniCArt(true);
+    } else {
+      this.setState({
+        message:
+        (<span id="message">This product is actually out of stock!</span>),
+      });
+    }
+  };
 
-      saveToStorage = (newProduct, colorKey, sizeKey) => {
-        let cartProduct = newProduct;
-        const colorSelected = colorKey;
-        const sizeSelected = sizeKey;
-        cartProduct = { ...cartProduct, colorSelected };
-        cartProduct = { ...cartProduct, sizeSelected };
-        localStorage.setItem('productToCheck', JSON.stringify(cartProduct));
-      }
+  saveToStorage = (newProduct, color, size) => {
+    let cartProduct = newProduct;
+    const colorSelected = color;
+    const sizeSelected0 = size;
+    const sizeSelected1 = size;
+    const sizeSelected2 = size;
+    cartProduct = {
+      ...cartProduct,
+      colorSelected,
+      sizeSelected0,
+      sizeSelected1,
+      sizeSelected2,
+    };
+    localStorage.setItem('productToCheck', JSON.stringify(cartProduct));
+  };
 
-      render() {
-        let sizeProduct = [];
-        let colorProduct = [];
+  render() {
+    let sizeProduct = [];
+    let colorProduct = [];
+    const newProduct = this.props.product.product;
 
-        const newProduct = this.props.product.product;
-
-        if (newProduct !== undefined) {
-          if (newProduct.attributes.length > 1) {
-            colorProduct = newProduct.attributes.filter((size) => size.name === 'Color');
-          }
-          sizeProduct = newProduct.attributes.filter((size) => size.name !== 'Color');
-        }
-        return (
-          <div
-            className="productShown"
-          >
-            <div
-              onClick={() => this.props.showMiniCArt(false)}
-              className={this.props.grayFilter ? 'filter-gray' : 'no-filter'}
-            />
-            {
-        newProduct
-          ? (
-            <div className="productContainer">
-              <div className="sideImages">
-                {
-                newProduct.gallery.map((pic, id) => (
-                  <img
-                    key={pic}
-                    src={pic}
-                    alt=""
-                    className="litleImg"
-                    onClick={() => this.setState({ imageShown: id })}
-                  />
-                ))
-              }
-              </div>
-              <div className="bigImgContainer">
-                <img src={newProduct.gallery[this.state.imageShown]} alt="" className="bigImage" />
-              </div>
-              <div className="detailsContainer">
-                <h3 id="titleBrand">{newProduct.brand}</h3>
-                <h4 id="titlePopUp">{newProduct.name}</h4>
-                {sizeProduct.length > 0 ? (<p className="sizeTitle">SIZE:</p>) : (<></>)}
-
-                {sizeProduct.length > 0
-                  ? (
-                    <div className="sizesContainer">
-                      {
-                  sizeProduct[0].items.map((size, key) => (
-                    <p
-                      key={size}
-                      onClick={() => {
-                        this.setState({ sizeSelected: key });
-                        this.saveToStorage(newProduct, this.state.colorSelected, key);
-                      }}
-                      className={this.state.sizeSelected === key ? 'chosenSize' : 'productSize'}
-                    >
-                      {size.value}
-                    </p>
-                  ))
-                }
-                    </div>
-                  ) : ''}
-                {colorProduct.length > 0 ? (<p className="sizeTitle">COLOR:</p>) : (<></>) }
-
-                {
-                colorProduct.length > 0 ? (
-                  <div className="colorsContainer">
-                    {
-                   colorProduct[0].items.map((color, key) => (
-                     <div
-                       key={color}
-                       onClick={() => {
-                         this.setState({ colorSelected: key });
-                         this.saveToStorage(newProduct, key, this.state.sizeSelected);
-                       }}
-                       className={this.state.colorSelected === key ? 'chosenColor' : 'Productcolor'}
-                       style={{ backgroundColor: color.value }}
-                     />
-                   ))
-                  }
-                  </div>
-                )
-                  : (<></>)
-               }
-                <p
-                  className="detailsPrice"
-                >
-                  {newProduct.prices[this.props.currencieId].currency.symbol}
-                  {newProduct.prices[this.props.currencieId].amount}
-                </p>
-                <button
-                  type="button"
-                  className="add_to_cart"
-                  onClick={() => this.addNewToCart(newProduct)}
-                >
-                  ADD TO CART
-                </button>
-                <div
-                  className="text-Description"
-                  dangerouslySetInnerHTML={{ __html: newProduct.description }}
-                />
-
-              </div>
-            </div>
-          )
-          : (<p>...fetching</p>)
-}
-          </div>
+    if (newProduct !== undefined) {
+      if (newProduct.attributes.length > 1) {
+        colorProduct = newProduct.attributes.filter(
+          (size) => size.name === 'Color',
         );
       }
+      sizeProduct = newProduct.attributes.filter(
+        (size) => size.name !== 'Color',
+      );
+    }
+    return (
+      <div className="productShown">
+        <div
+          onClick={() => this.props.showMiniCArt(false)}
+          className={this.props.grayFilter ? 'filter-gray' : 'no-filter'}
+        />
+        {newProduct ? (
+          <div className="productContainer">
+            <div className="sideImages">
+              {newProduct.gallery.map((pic, id) => (
+                <img
+                  key={pic}
+                  src={pic}
+                  alt=""
+                  className="litleImg"
+                  onClick={() => this.setState({ imageShown: id })}
+                />
+              ))}
+            </div>
+            <div className="bigImgContainer">
+              <img
+                src={newProduct.gallery[this.state.imageShown]}
+                alt=""
+                className="bigImage"
+              />
+            </div>
+            <div className="detailsContainer">
+              <h3 id="titleBrand">{newProduct.brand}</h3>
+              <h4 id="titlePopUp">{newProduct.name}</h4>
+
+              {sizeProduct.length > 0 ? sizeProduct.map((child, i) => (
+                (
+                  <>
+                    <p className="sizeTitle">
+                      {sizeProduct[i].id}
+                      :
+                    </p>
+                    <div className="sizesContainer" key={child}>
+
+                      {sizeProduct[i].items.map((size, key) => (
+                        <p
+                          key={size}
+                          onClick={() => {
+                            this.setState({ sizeSelected: key });
+                            this.setState({ [`sizeSelected${i}`]: key });
+                            this.saveToStorage(
+                              newProduct,
+                              this.state.colorSelected,
+                              key,
+                            );
+                          }}
+                          className={
+                          this.state[`sizeSelected${i}`] === key
+                            ? 'chosenSize'
+                            : 'productSize'
+                        }
+                        >
+                          {size.value}
+                        </p>
+                      ))}
+                    </div>
+                  </>
+                )
+              ))
+                : ('')}
+
+              {colorProduct.length > 0 ? (
+                <>
+                  <p className="sizeTitle">
+                    {colorProduct[0].id}
+                    :
+                  </p>
+                  <div className="colorsContainer">
+                    {colorProduct[0].items.map((color, key) => (
+                      <div
+                        key={color}
+                        onClick={() => {
+                          this.setState({ colorSelected: key });
+                          this.saveToStorage(
+                            newProduct,
+                            key,
+                            this.state.sizeSelected0,
+                          );
+                        }}
+                        className={
+                        this.state.colorSelected === key
+                          ? 'chosenColor'
+                          : 'Productcolor'
+                      }
+                        style={{ backgroundColor: color.value }}
+                      />
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+              <p className="detailsPrice">
+                {newProduct.prices[this.props.currencieId].currency.symbol}
+                {newProduct.prices[this.props.currencieId].amount.toFixed(2)}
+              </p>
+              <button
+                type="button"
+                className="add_to_cart"
+                onClick={() => this.addNewToCart(newProduct)}
+              >
+                ADD TO CART
+              </button>
+              {this.state.message}
+              <Description description={newProduct.description} />
+            </div>
+          </div>
+        ) : (
+          <p>...fetching</p>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({ product: state.productReducer });
@@ -173,7 +218,6 @@ const mapStateToProps = (state) => ({ product: state.productReducer });
 const mapDispatchToProps = (dispatch) => ({
   addTocard: (product) => dispatch(addTocard(product)),
   fetchProduct: (id) => dispatch(fetchProduct(id)),
-  changeStoredStatus: (item) => dispatch(changeStoredStatus(item)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
